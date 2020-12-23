5e6d7079d4c5607a3f6c20e9115dae2972c239a9
brian <bmagsalan@gmail.com>
2020-12-23 14:39:37 -0500

    Added bitmap scanning.

---
+++ mediapipe/examples/android/src/java/com/google/mediapipe/apps/handtrackinggpu/BmpProducer.java
@@ -0,0 +1,53 @@
+package com.google.mediapipe.apps.handtrackinggpu;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.widget.Toast;
+
+
+public class BmpProducer extends Thread {
+
+    private final Context context;
+    CustomFrameAvailableListner customFrameAvailableListner;
+
+    public int height = 513,width = 513;
+    Bitmap bmp;
+
+    BmpProducer(Context context){
+        Toast.makeText(context, "BmpProducer", Toast.LENGTH_SHORT).show();
+        this.context = context;
+        bmp = BitmapFactory.decodeResource(context.getResources(), R.drawable.img2);
+        bmp = Bitmap.createScaledBitmap(bmp,480,640,true);
+        height = bmp.getHeight();
+        width = bmp.getWidth();
+        start();
+    }
+
+    public void setCustomFrameAvailableListner(CustomFrameAvailableListner customFrameAvailableListner){
+        this.customFrameAvailableListner = customFrameAvailableListner;
+    }
+
+    public static final String TAG="BmpProducer";
+    @Override
+    public void run() {
+        super.run();
+        while ((true)){
+            if(bmp==null || customFrameAvailableListner == null)
+                continue;
+            Log.d(TAG,"Writing frame");
+            customFrameAvailableListner.onFrame(bmp);
+            /*OTMainActivity.imageView.post(new Runnable() {
+                @Override
+                public void run() {
+                    OTMainActivity.imageView.setImageBitmap(bg);
+                }
+            });*/
+            try{
+                Thread.sleep(10);
+            }catch (Exception e){
+                Log.d(TAG,e.toString());
+            }
+        }
+    }
+}
