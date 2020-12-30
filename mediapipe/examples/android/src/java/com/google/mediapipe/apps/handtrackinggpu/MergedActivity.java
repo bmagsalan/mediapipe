109bb438124201fc2260294e8643e0bf6ddfed05
brian <bmagsalan@gmail.com>
2020-12-30 13:50:21 -0500

    Added merged activity.

--- mediapipe/examples/android/src/java/com/google/mediapipe/apps/handtrackinggpu/MergedActivity.java
+++ mediapipe/examples/android/src/java/com/google/mediapipe/apps/handtrackinggpu/MergedActivity.java
@@ -1,13 +1,30 @@
 package com.google.mediapipe.apps.handtrackinggpu;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.google.mediapipe.components.FrameProcessor;
+import com.google.mediapipe.formats.proto.LandmarkProto;
 import com.google.mediapipe.framework.AndroidAssetUtil;
+import com.google.mediapipe.framework.AndroidPacketCreator;
+import com.google.mediapipe.framework.Packet;
+import com.google.mediapipe.framework.PacketGetter;
+import com.google.mediapipe.glutil.EglManager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Direct the Camera preview to a GLES texture and manipulate it.
@@ -69,6 +86,43 @@ public class MergedActivity extends Activity implements SurfaceHolder.Callback {
     private CameraActivity.MainHandler mHandler;
 
 
+    private static final String BINARY_GRAPH_NAME = "hand_tracking_mobile_gpu.binarypb";
+    private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
+    private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
+    private static final String INPUT_NUM_HANDS_SIDE_PACKET_NAME = "num_hands";
+    private static final String OUTPUT_LANDMARKS_STREAM_NAME = "hand_landmarks";
+    private static final int NUM_HANDS = 2;
+    private static final boolean FLIP_FRAMES_VERTICALLY = true;
+
+    static {
+        // Load all native libraries needed by the app.
+        System.loadLibrary("mediapipe_jni");
+//        try {
+        System.loadLibrary("opencv_java3");
+//        } catch (java.lang.UnsatisfiedLinkError e) {
+//            // Some example apps (e.g. template matching) require OpenCV 4.
+//            System.loadLibrary("opencv_java4");
+//        }
+    }
+
+    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
+    private SurfaceView previewDisplayView;
+
+    // Creates and manages an {@link EGLContext}.
+    private EglManager eglManager;
+    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
+    // frames onto a {@link Surface}.
+    private FrameProcessor processor;
+    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
+    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
+    private BitmapConverter converter;
+
+    // Handles camera access via the {@link CameraX} Jetpack support library.
+    //private CameraXPreviewHelper cameraHelper;
+    BmpProducer bitmapProducer;
+    private int imgCounter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,7 +134,72 @@ public class MergedActivity extends Activity implements SurfaceHolder.Callback {
         SurfaceHolder sh = sv.getHolder();
         sh.addCallback(this);
 
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(this);
+
+        previewDisplayView = new SurfaceView(this);
+        previewDisplayView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1));
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.root_view);
+        rootView.addView(previewDisplayView);
+
+        previewDisplayView
+                .getHolder()
+                .addCallback(
+                        new SurfaceHolder.Callback() {
+                            @Override
+                            public void surfaceCreated(SurfaceHolder holder) {
+                                processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
+                            }
+
+                            @Override
+                            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                                bitmapProducer.setCustomFrameAvailableListner(converter);
+                            }
+
+                            @Override
+                            public void surfaceDestroyed(SurfaceHolder holder) {
+                                processor.getVideoSurfaceOutput().setSurface(null);
+                            }
+                        });
+
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(this);
+
+        eglManager = new EglManager(null);
+        processor =
+                new FrameProcessor(
+                        this,
+                        eglManager.getNativeContext(),
+                        BINARY_GRAPH_NAME,
+                        INPUT_VIDEO_STREAM_NAME,
+                        OUTPUT_VIDEO_STREAM_NAME);
+        processor.getVideoSurfaceOutput().setFlipY(FLIP_FRAMES_VERTICALLY);
+
+        processor.addPacketCallback(
+                OUTPUT_LANDMARKS_STREAM_NAME,
+                (packet) -> {
+                    Log.d(TAG, "Received multi-hand landmarks packet.");
+                    List<LandmarkProto.NormalizedLandmarkList> multiHandLandmarks =
+                            PacketGetter.getProtoVector(packet, LandmarkProto.NormalizedLandmarkList.parser());
+                    Log.d(
+                            TAG,
+                            "[TS:"
+                                    + packet.getTimestamp()
+                                    + "] "
+                                    + getMultiHandLandmarksDebugString(multiHandLandmarks));
+                });
+
+        AndroidPacketCreator packetCreator = processor.getPacketCreator();
+        Map<String, Packet> inputSidePackets = new HashMap<>();
+        inputSidePackets.put(INPUT_NUM_HANDS_SIDE_PACKET_NAME, packetCreator.createInt32(NUM_HANDS));
+        processor.setInputSidePackets(inputSidePackets);
+
+
     }
+
+
 
     @Override
     protected void onResume() {
@@ -105,6 +224,18 @@ public class MergedActivity extends Activity implements SurfaceHolder.Callback {
             Log.d(TAG, "No previous surface");
         }
         Log.d(TAG, "onResume END");
+
+        converter = new BitmapConverter(eglManager.getContext());
+        converter.setConsumer(processor);
+        bitmapProducer = new BmpProducer(this);
+
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                loadImage(null);
+            }
+        },1000,1000);
     }
 
     @Override
@@ -125,6 +256,54 @@ public class MergedActivity extends Activity implements SurfaceHolder.Callback {
         }
         mRenderThread = null;
         Log.d(TAG, "onPause END");
+    }
+
+    public void loadImage(View view) {
+        Bitmap bmp = null;
+        if( imgCounter % 5 == 0) {
+            bmp = BitmapFactory.decodeResource(getResources(), R.drawable.img3);
+        }else if( imgCounter % 5 == 1){
+            bmp = BitmapFactory.decodeResource(getResources(), R.drawable.img4);
+        }else if( imgCounter % 5 == 2){
+            bmp = BitmapFactory.decodeResource(getResources(), R.drawable.img1);
+        }else if( imgCounter % 5 == 3){
+            bmp = BitmapFactory.decodeResource(getResources(), R.drawable.img5);
+        }else if( imgCounter % 5 == 4){
+            bmp = BitmapFactory.decodeResource(getResources(), R.drawable.img2);
+        }
+
+        bmp = Bitmap.createScaledBitmap(bmp,480,640,true);
+        bitmapProducer.loadBitmaps(UtilsBitmap.bitmapToRgba(bmp),bmp.getWidth(), bmp.getHeight());
+
+        imgCounter++;
+    }
+
+    private String getMultiHandLandmarksDebugString(List<LandmarkProto.NormalizedLandmarkList> multiHandLandmarks) {
+        if (multiHandLandmarks.isEmpty()) {
+            return "No hand landmarks";
+        }
+        String multiHandLandmarksStr = "Number of hands detected: " + multiHandLandmarks.size() + "\n";
+        int handIndex = 0;
+        for (LandmarkProto.NormalizedLandmarkList landmarks : multiHandLandmarks) {
+            multiHandLandmarksStr +=
+                    "\t#Hand landmarks for hand[" + handIndex + "]: " + landmarks.getLandmarkCount() + "\n";
+            int landmarkIndex = 0;
+            for (LandmarkProto.NormalizedLandmark landmark : landmarks.getLandmarkList()) {
+                multiHandLandmarksStr +=
+                        "\t\tLandmark ["
+                                + landmarkIndex
+                                + "]: ("
+                                + landmark.getX()
+                                + ", "
+                                + landmark.getY()
+                                + ", "
+                                + landmark.getZ()
+                                + ")\n";
+                ++landmarkIndex;
+            }
+            ++handIndex;
+        }
+        return multiHandLandmarksStr;
     }
 
     @Override   // SurfaceHolder.Callback
